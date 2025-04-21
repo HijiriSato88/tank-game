613936837b5acea7feab357c830ed99fee3879b7
HijiriSato88 <satohiji0808@outlook.com>
2025-04-21 11:11:23 +0900

    envloadがdbフォルダにあるの違和感あったので修正

--- backend/main.go
+++ backend/main.go
@@ -1,16 +1,25 @@
 package main
 
 import (
+	"log"
+
 	"backend/db"
 	"backend/handler"
 	"backend/pkg/jwtutil"
+
+	"github.com/joho/godotenv"
 
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
-	db.Initialize()
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	db.SetupDB()
+
 	e := echo.New()
 
 	e.POST("/signup", handler.Signup)
