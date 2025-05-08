43f4da0bafaa04839b48e1ca16c98cc242c93624
HijiriSato88 <satohiji0808@outlook.com>
2025-05-08 16:29:57 +0900

    ZREVRANGEでScoreランキングを降順で取得

--- backend/domain/model/ranking.go
+++ backend/domain/model/ranking.go
@@ -1,7 +1,6 @@
 package model
 
 type RankingEntry struct {
-	UserID    int    `db:"id" json:"user_id"`
 	Username  string `db:"username" json:"username"`
 	HighScore int    `db:"high_score" json:"high_score"`
 }
--- backend/infra/ranking.go
+++ backend/infra/ranking.go
@@ -4,6 +4,7 @@ import (
 	"backend/db"
 	"backend/domain/model"
 	"backend/domain/repository"
+
 	"github.com/redis/go-redis/v9"
 )
 
@@ -13,16 +14,38 @@ func NewRankingRepository() repository.RankingRepository {
 	return &rankingRepository{}
 }
 
+// MySQL からランキングを取得する : limit=1万だとindexを貼っても10万行(ALL)読み取ってた
+// func (r *rankingRepository) GetRanking(limit int) ([]model.RankingEntry, error) {
+// 	var rankings []model.RankingEntry
+// 	err := db.DB.Select(&rankings, `
+// 		SELECT id, username, high_score
+// 		FROM users
+// 		WHERE high_score > 0
+// 		ORDER BY high_score DESC
+// 		LIMIT ?
+// 	`, limit)
+// 	return rankings, err
+// }
+
 func (r *rankingRepository) GetRanking(limit int) ([]model.RankingEntry, error) {
-	var rankings []model.RankingEntry
-	err := db.DB.Select(&rankings, `
-		SELECT id, username, high_score
-		FROM users
-		WHERE high_score > 0
-		ORDER BY high_score DESC
-		LIMIT ?
-	`, limit)
-	return rankings, err
+	zs, err := db.Redis.ZRevRangeWithScores(ctx, "ranking", 0, int64(limit-1)).Result()
+	if err != nil {
+		return nil, err
+	}
+
+	var result []model.RankingEntry
+	for _, z := range zs {
+		username, ok := z.Member.(string)
+		if !ok {
+			continue
+		}
+		result = append(result, model.RankingEntry{
+			Username:  username,
+			HighScore: int(z.Score),
+		})
+	}
+
+	return result, nil
 }
 
 func (r *rankingRepository) ZAddScore(username string, score int) error {
